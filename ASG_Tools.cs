6ef03833b01b55fc7dd0470ee9f52bb06410789f
Antonios Hadjigeorgalis <Antonios@tradingtechnologies.com>
2013-12-02 15:55:51 -0500

    initial commit

---
+++ ASG_Tools.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ASG_Tools
+{ 
+    using TradingTechnologies.TTAPI;
+
+    public class ASG
+    {
+        public MarketKey mkt(string market)
+        {
+            switch (market.ToUpper())
+            {
+                case "CME":
+                    return MarketKey.Cme;
+                case "ICE":
+                    return MarketKey.Ice;
+                default:
+                    return MarketKey.Invalid;
+            }
+        }
+
+        public ProductType prodtype(string pt)
+        {
+            switch (pt.ToUpper())
+            {
+                case "FUTURE":
+                    return ProductType.Future;
+                case "SPREAD":
+                    return ProductType.Spread;
+                default:
+                    return ProductType.Invalid;
+            }
+        }
+
+    }
+}
---
+++ start.cmd
@@ -0,0 +1,3 @@
+set user=Antonios
+set pw=1234
+start TTAPI_Sample_Console_ASEOrderRouting.exe %user% %pw% 
